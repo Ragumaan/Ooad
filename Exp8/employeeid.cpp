1d917cb0dbea45a7f361c7a83202b9dc033c1abb
Ragumaan <112238424+Ragumaan@users.noreply.github.com>
2023-08-21 20:59:43 +0530

    Add files via upload

---
+++ Exp8/employeeid.cpp
@@ -0,0 +1,26 @@
+#include "employeeid.h"
+
+// Constructors/Destructors
+//  
+
+employeeid::employeeid () {
+initAttributes();
+}
+
+employeeid::~employeeid () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void employeeid::initAttributes () {
+}
+
