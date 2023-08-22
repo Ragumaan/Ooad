f70895f7e34f2c1b3a0ff0f3da84791c3fc63e35
Ragumaan <112238424+Ragumaan@users.noreply.github.com>
2023-08-22 08:36:30 +0530

    Add files via upload

---
+++ Exp14/catalog.cpp
@@ -0,0 +1,26 @@
+#include "catalog.h"
+
+// Constructors/Destructors
+//  
+
+catalog::catalog () {
+initAttributes();
+}
+
+catalog::~catalog () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void catalog::initAttributes () {
+}
+
