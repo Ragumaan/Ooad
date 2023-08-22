b2e98814101b6bb0c4ff661ba175986c312774d1
Ragumaan <112238424+Ragumaan@users.noreply.github.com>
2023-08-22 16:25:12 +0530

    Add files via upload

---
+++ Exp31/Loanstatus.cpp
@@ -0,0 +1,23 @@
+#include "Loanstatus.h"
+
+// Constructors/Destructors
+//  
+
+Loanstatus::Loanstatus () {
+}
+
+Loanstatus::~Loanstatus () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+
---
+++ Exp31/agricultureloanmanagement.cpp
@@ -0,0 +1,26 @@
+#include "agricultureloanmanagement.h"
+
+// Constructors/Destructors
+//  
+
+agricultureloanmanagement::agricultureloanmanagement () {
+initAttributes();
+}
+
+agricultureloanmanagement::~agricultureloanmanagement () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void agricultureloanmanagement::initAttributes () {
+}
+
