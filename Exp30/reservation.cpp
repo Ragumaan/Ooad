1f8f1b54f9b2fb5f34e6cf529feaab076169aacf
Ragumaan <112238424+Ragumaan@users.noreply.github.com>
2023-08-22 16:12:43 +0530

    Add files via upload

---
+++ Exp30/reservation.cpp
@@ -0,0 +1,26 @@
+#include "Reservation.h"
+
+// Constructors/Destructors
+//  
+
+Reservation::Reservation () {
+initAttributes();
+}
+
+Reservation::~Reservation () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void Reservation::initAttributes () {
+}
+
---
+++ Exp30/traintickectreservationsystem.cpp
@@ -0,0 +1,26 @@
+#include "traintickectreservationsystem.h"
+
+// Constructors/Destructors
+//  
+
+traintickectreservationsystem::traintickectreservationsystem () {
+initAttributes();
+}
+
+traintickectreservationsystem::~traintickectreservationsystem () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void traintickectreservationsystem::initAttributes () {
+}
+
